8d0fb0bd424969a63557513733884e3bd58e40a2
Christophe Boulanger <christophe.boulanger@imio.be>
2021-02-09 08:42:29 +0100

    Add controlpanel, some keys in registry (to keep ia.ts credentials), vocabularies and utils (to access ia.ts api)

---
+++ src/imio/smartweb/core/browser/controlpanel.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+from imio.smartweb.core import _
+from plone.app.registry.browser.controlpanel import ControlPanelFormWrapper
+from plone.app.registry.browser.controlpanel import RegistryEditForm
+from plone.z3cform import layout
+from zope import schema
+from zope.interface import Interface
+
+
+class ISmartwebControlPanel(Interface):
+
+    url_formdefs_api = schema.TextLine(
+        title=_(u"Url to get forms from your e-guichet"),
+        description=_(
+            u"url to get forms from your e-guichet. \r\n (Seems like : https://COMMUNE-formulaires.guichet-citoyen.be/api/formdefs/)"
+        ),
+        required=False,
+    )
+
+    secret_key_api = schema.Password(
+        title=_(u"Secret key"), description=_(u"Secret key to use API"), required=False,
+    )
+
+
+class SmartwebControlPanelForm(RegistryEditForm):
+    schema = ISmartwebControlPanel
+    schema_prefix = "smartweb"
+    label = u"Smartweb Settings"
+
+
+SmartwebControlPanelView = layout.wrap_form(
+    SmartwebControlPanelForm, ControlPanelFormWrapper
+)
---
+++ src/imio/smartweb/core/vocabularies.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+from plone import api
+from zope.schema.vocabulary import SimpleTerm
+from zope.schema.vocabulary import SimpleVocabulary
+import json
+import requests
+
+# from imio.behavior.teleservices.config import API_HEADERS
+# from imio.behavior.teleservices.utils import get_api_url_for_meetings
+from imio.smartweb.core.contents.procedure.utils import sign_url
+
+
+class RemoteProceduresVocabularyFactory:
+    def __call__(self, context):
+        # sample : "https://olln-formulaires.guichet-citoyen.be/api/formdefs/"
+        url = api.portal.get_registry_record("smartweb.url_formdefs_api")
+        # sample : "568DGess2x8j8twv7x2Y2MApjn789xfG7jM27r399q4xSD27Jz"
+        key = api.portal.get_registry_record("smartweb.secret_key_api")
+        orig = "ia.smartweb"
+        if not url:
+            return SimpleVocabulary([])
+        query_full = sign_url(url, key, orig)
+        try:
+            response = requests.get(query_full)
+        except Exception:
+            return SimpleVocabulary([])
+
+        if response.status_code != 200:
+            return SimpleVocabulary([])
+
+        json_procedures = json.loads(response.text)
+        return SimpleVocabulary(
+            [
+                SimpleTerm(value=elem["url"], title=elem["title"])
+                for elem in json_procedures.get("data", [])
+            ]
+        )
+
+
+RemoteProceduresVocabulary = RemoteProceduresVocabularyFactory()
