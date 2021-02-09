40a67f85fc3e2857114fb2874899ff156cd0b4f2
Benoit Suttor <bsuttor@imio.be>
2021-02-09 10:09:55 +0100

    tests: fixing procedure

--- src/imio/smartweb/core/tests/test_ct_procedure.py
+++ src/imio/smartweb/core/tests/test_ct_procedure.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from imio.smartweb.core.content.procedure import IProcedure  # NOQA E501
+from imio.smartweb.core.contents import IProcedure  # NOQA E501
 from imio.smartweb.core.testing import IMIO_SMARTWEB_CORE_INTEGRATION_TESTING  # noqa
 from plone import api
 from plone.app.testing import setRoles
@@ -11,72 +11,60 @@ from zope.component import queryUtility
 import unittest
 
 
-
-
 class ProcedureIntegrationTest(unittest.TestCase):
 
     layer = IMIO_SMARTWEB_CORE_INTEGRATION_TESTING
 
     def setUp(self):
         """Custom shared utility setup for tests."""
-        self.portal = self.layer['portal']
-        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+        self.portal = self.layer["portal"]
+        setRoles(self.portal, TEST_USER_ID, ["Manager"])
         portal_types = self.portal.portal_types
         parent_id = portal_types.constructContent(
-            'imio.smartweb.Folder',
+            "imio.smartweb.Folder",
             self.portal,
-            'parent_container',
-            title='Parent container',
+            "parent_container",
+            title="Parent container",
         )
         self.parent = self.portal[parent_id]
 
     def test_ct_procedure_schema(self):
-        fti = queryUtility(IDexterityFTI, name='Procedure')
+        fti = queryUtility(IDexterityFTI, name="imio.smartweb.Procedure")
         schema = fti.lookupSchema()
         self.assertEqual(IProcedure, schema)
 
     def test_ct_procedure_fti(self):
-        fti = queryUtility(IDexterityFTI, name='Procedure')
+        fti = queryUtility(IDexterityFTI, name="imio.smartweb.Procedure")
         self.assertTrue(fti)
 
     def test_ct_procedure_factory(self):
-        fti = queryUtility(IDexterityFTI, name='Procedure')
+        fti = queryUtility(IDexterityFTI, name="imio.smartweb.Procedure")
         factory = fti.factory
         obj = createObject(factory)
 
         self.assertTrue(
-            IProcedure.providedBy(obj),
-            u'IProcedure not provided by {0}!'.format(
-                obj,
-            ),
+            IProcedure.providedBy(obj), u"IProcedure not provided by {0}!".format(obj,),
         )
 
     def test_ct_procedure_adding(self):
-        setRoles(self.portal, TEST_USER_ID, ['Contributor'])
+        setRoles(self.portal, TEST_USER_ID, ["Contributor"])
         obj = api.content.create(
-            container=self.parent,
-            type='Procedure',
-            id='procedure',
+            container=self.parent, type="imio.smartweb.Procedure", id="procedure",
         )
 
         self.assertTrue(
             IProcedure.providedBy(obj),
-            u'IProcedure not provided by {0}!'.format(
-                obj.id,
-            ),
+            u"IProcedure not provided by {0}!".format(obj.id,),
         )
 
         parent = obj.__parent__
-        self.assertIn('procedure', parent.objectIds())
+        self.assertIn("procedure", parent.objectIds())
 
         # check that deleting the object works too
         api.content.delete(obj=obj)
-        self.assertNotIn('procedure', parent.objectIds())
+        self.assertNotIn("procedure", parent.objectIds())
 
     def test_ct_procedure_globally_not_addable(self):
-        setRoles(self.portal, TEST_USER_ID, ['Contributor'])
-        fti = queryUtility(IDexterityFTI, name='Procedure')
-        self.assertFalse(
-            fti.global_allow,
-            u'{0} is globally addable!'.format(fti.id)
-        )
+        setRoles(self.portal, TEST_USER_ID, ["Contributor"])
+        fti = queryUtility(IDexterityFTI, name="imio.smartweb.Procedure")
+        self.assertTrue(fti.global_allow, u"{0} is globally addable!".format(fti.id))
