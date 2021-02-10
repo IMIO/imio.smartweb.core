306f4e5d4efa7f3a2c022bf2bcf4e47b006aeaaa
Christophe Boulanger <christophe.boulanger@imio.be>
2021-02-10 11:14:57 +0100

    Change invariant browser tests to direct form tests.

--- src/imio/smartweb/core/tests/test_behaviors.py
+++ src/imio/smartweb/core/tests/test_behaviors.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from imio.smartweb.core.behaviors.procedure import IProcedure  # NOQA E501
+from imio.smartweb.core.browser.procedure.add import AddForm
 from imio.smartweb.core.contents import IProcedure  # NOQA E501
 from imio.smartweb.core.testing import IMIO_SMARTWEB_CORE_FUNCTIONAL_TESTING  # noqa
 from imio.smartweb.core.tests.utils import get_procedure_json
@@ -10,10 +11,13 @@ from plone.app.testing import setRoles
 from plone.app.testing import SITE_OWNER_NAME
 from plone.app.testing import SITE_OWNER_PASSWORD
 from plone.app.testing import TEST_USER_ID
+from plone.app.z3cform.interfaces import IPloneFormLayer
 from plone.dexterity.interfaces import IDexterityFTI
 from plone.testing.z2 import Browser
 from zope.component import createObject
 from zope.component import queryUtility
+from zope.interface import alsoProvides
+from zope.publisher.browser import TestRequest
 
 import json
 import os
@@ -51,32 +55,44 @@ class ProcedureFunctionalTest(unittest.TestCase):
 
     @requests_mock.Mocker()
     def test_procedure_invariant(self, m):
-        folder = api.content.create(
-            container=self.portal,
-            type="imio.smartweb.Folder",
-            id="folder",
-        )
-        transaction.commit()
         m.get(
             "https://demo.guichet-citoyen.be/api/formdefs/",
             text=json.dumps(self.json_procedures_raw_mock),
         )
-        self.browser.open(folder.absolute_url() + "/++add++imio.smartweb.Procedure")
-        self.browser.getControl("Save").click()
-        self.assertIn("Procedure field is required !", self.browser.contents)
-
-        self.browser.getControl(name="form.widgets.IBasic.title").value = "My Procedure"
-        self.browser.getControl(
-            name="form.widgets.IProcedure.procedure_url"
-        ).value = "http://another_e_guichet.be/procedure1"
-        self.browser.getControl(
-            name="form.widgets.IProcedure.procedure_ts:list"
-        ).value = "https://demo-formulaires.guichet-citoyen.be/acte-de-deces/"
-        self.browser.getControl("Save").click()
-        self.assertIn("Only one procedure field can be filled !", self.browser.contents)
-
-        self.browser.getControl(name="form.widgets.IProcedure.procedure_url").value = ""
-        self.browser.getControl("Save").click()
-        self.assertIn(
-            '<h1 class="documentFirstHeading">My Procedure</h1>', self.browser.contents
+        request = TestRequest(
+            form={
+                "form.widgets.IBasic.title": "My Procedure",
+                "form.widgets.IProcedure.procedure_url": "http://another_e_guichet.be/procedure1",
+            }
         )
+        alsoProvides(request, IPloneFormLayer)
+        form = AddForm(self.portal, request)
+        form.update()
+        data, errors = form.extractData()
+        self.assertTrue(len(errors) == 0)
+
+        request = TestRequest(
+            form={
+                "form.widgets.IBasic.title": "My Procedure",
+            }
+        )
+        alsoProvides(request, IPloneFormLayer)
+        form = AddForm(self.portal, request)
+        form.update()
+        data, errors = form.extractData()
+        self.assertTrue(len(errors) == 1)
+        self.assertIn(errors[0].message, 'Procedure field is required !')
+
+        request = TestRequest(
+            form={
+                "form.widgets.IBasic.title": "My Procedure",
+                "form.widgets.IProcedure.procedure_url": "http://another_e_guichet.be/procedure1",
+                "form.widgets.IProcedure.procedure_ts":"https://demo-formulaires.guichet-citoyen.be/acte-de-deces/",
+            }
+        )
+        alsoProvides(request, IPloneFormLayer)
+        form = AddForm(self.portal, request)
+        form.update()
+        data, errors = form.extractData()
+        self.assertTrue(len(errors) == 1)
+        self.assertIn(errors[0].message, 'Only one procedure field can be filled !')
