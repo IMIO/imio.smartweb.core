a21ba122a1f417d2e47af2308e175477835b775c
Christophe Boulanger <christophe.boulanger@imio.be>
2021-03-08 10:01:23 +0100

    Create "exclude from parent listing" behavior and its index.

---
+++ src/imio/smartweb/core/behaviors/listing.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+from imio.smartweb.locales import SmartwebMessageFactory as _
+from plone.autoform import directives
+from plone.autoform.interfaces import IFormFieldProvider
+from plone.supermodel import model
+from zope import schema
+from zope.interface import provider
+
+
+@provider(IFormFieldProvider)
+class IListing(model.Schema):
+
+    directives.order_after(
+        exclude_from_parent_listing="IQuickAccess.include_in_quick_access"
+    )
+    model.fieldset("settings", fields=["exclude_from_parent_listing"])
+    exclude_from_parent_listing = schema.Bool(
+        title=_(u"Exclude from parent listing"),
+        description=_(
+            u"If selected, this item will not appear in parent listing views"
+        ),
+        required=False,
+        default=False,
+    )
