89bbbf6de9b7a0ad9fe844baa68f6e4be780bb9e
Christophe Boulanger <christophe.boulanger@imio.be>
2021-03-08 14:11:41 +0100

    blacked file

--- src/imio/smartweb/core/contents/folder/views.py
+++ src/imio/smartweb/core/contents/folder/views.py
@@ -5,18 +5,18 @@ from plone.app.contenttypes.browser.folder import FolderView as BaseFolderView
 
 
 class FolderView(BaseFolderView):
+    """"""
 
     def results(self, **kwargs):
         # Extra filter
-        kwargs.update(self.request.get('contentFilter', {}))
-        kwargs.setdefault('batch', True)
-        kwargs.setdefault('b_size', self.b_size)
-        kwargs.setdefault('b_start', self.b_start)
-        kwargs.setdefault('orphan', 1)
-        kwargs.setdefault('exclude_from_parent_listing', False)
+        kwargs.update(self.request.get("contentFilter", {}))
+        kwargs.setdefault("batch", True)
+        kwargs.setdefault("b_size", self.b_size)
+        kwargs.setdefault("b_start", self.b_start)
+        kwargs.setdefault("orphan", 1)
+        kwargs.setdefault("exclude_from_parent_listing", False)
 
-        listing = aq_inner(self.context).restrictedTraverse(
-            '@@folderListing', None)
+        listing = aq_inner(self.context).restrictedTraverse("@@folderListing", None)
         if listing is None:
             return []
         results = listing(**kwargs)
