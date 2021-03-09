79e4daf0472acf26fc9ba5805624e3a63c4ab64e
Laurent Lasudry <lasudry@gmail.com>
2021-03-09 09:27:18 +0100

    Add conditional sections in Page view

--- src/imio/smartweb/core/contents/page/views.py
+++ src/imio/smartweb/core/contents/page/views.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
+
 from plone.dexterity.browser.view import DefaultView
 
 
 class PageView(DefaultView):
     """Page view"""
+
+    def available_section(self, section):
+        """
+        Check if specified section must be shown
+        """
+        return section in self.context.visible_sections
