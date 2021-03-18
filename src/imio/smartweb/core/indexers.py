733ac02045c332e36692bdb145e4c1c7e3a6cca1
Christophe Boulanger <christophe.boulanger@imio.be>
2021-03-18 11:27:43 +0100

    Add SearchableText indexer for imio.smartweb.SectionText

--- src/imio/smartweb/core/indexers.py
+++ src/imio/smartweb/core/indexers.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 
+from imio.smartweb.core.contents import ISectionText
 from plone.app.contenttypes.behaviors.leadimage import ILeadImage
+from plone.app.contenttypes.indexers import _unicode_save_string_concat
+from plone.app.contenttypes.indexers import SearchableText
 from plone.dexterity.interfaces import IDexterityContent
 from plone.indexer.decorator import indexer
 
@@ -10,3 +13,8 @@ def has_leadimage(obj):
     if ILeadImage.providedBy(obj) and getattr(obj, "image", False):
         return True
     return False
+
+
+@indexer(ISectionText)
+def SearchableText_sectiontext(obj):
+    return _unicode_save_string_concat(SearchableText(obj))
