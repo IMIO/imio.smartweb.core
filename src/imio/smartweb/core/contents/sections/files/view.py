3c64a2a00b0156fc15b02632b207dbb1cf1a91ec
Christophe Boulanger <christophe.boulanger@imio.be>
2021-03-18 14:42:52 +0100

    Add description and size of files in SectionFiles view.

--- src/imio/smartweb/core/contents/sections/files/view.py
+++ src/imio/smartweb/core/contents/sections/files/view.py
@@ -2,6 +2,7 @@
 
 from zope.component import queryMultiAdapter
 from imio.smartweb.core.contents.sections.views import SectionView
+from Products.CMFPlone.utils import human_readable_size
 
 
 class FilesView(SectionView):
@@ -15,3 +16,6 @@ class FilesView(SectionView):
     def get_mime_type_icon(self, file_obj):
         view = queryMultiAdapter((self.context, self.request), name="contenttype_utils")
         return view.getMimeTypeIcon(file_obj.file)
+
+    def human_readable_size(self, file_obj):
+        return human_readable_size(file_obj.file.getSize())
