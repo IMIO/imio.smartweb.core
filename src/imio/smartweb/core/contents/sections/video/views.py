5e4a56c08b6cb25ea2c5bd345d9cd14146158084
Laurent Lasudry <lasudry@gmail.com>
2021-03-19 16:09:51 +0100

    Blacked

--- src/imio/smartweb/core/contents/sections/video/views.py
+++ src/imio/smartweb/core/contents/sections/video/views.py
@@ -5,7 +5,6 @@ from imio.smartweb.core.contents.sections.views import SectionView
 
 
 class VideoView(SectionView):
-
     def get_embed_video(self):
         embedder = Embedder(width=800, height=600)
         return embedder(self.context.video_url, params=dict(autoplay=False))
