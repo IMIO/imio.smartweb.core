4c4953ce883fe79771ef148878608e4dd70c6339
Laurent Lasudry <lasudry@gmail.com>
2021-04-04 13:25:16 +0200

    Fix parent retrieval code

--- src/imio/smartweb/core/contents/pages/subscriber.py
+++ src/imio/smartweb/core/contents/pages/subscriber.py
@@ -6,5 +6,5 @@ from zope.interface import noLongerProvides
 def remove_default_page(obj, event):
     """Remove "pages" (i.s.Procedure, i.s.Page) marked as a default page out of a imio.smartweb.Folder"""
     noLongerProvides(obj, IDefaultPages)
-    folder = obj.parent()
+    folder = obj.aq_parent
     folder.default_page_uid = None
