39f4fa4399db54a4d263e742ad2c9c60e0949b55
Laurent Lasudry <lasudry@gmail.com>
2021-04-09 15:41:55 +0200

    Allow to choose scale for Gallery / Links sections

--- src/imio/smartweb/core/contents/sections/gallery/content.py
+++ src/imio/smartweb/core/contents/sections/gallery/content.py
@@ -2,11 +2,20 @@
 
 from imio.smartweb.core.contents.sections.base import ISection
 from imio.smartweb.core.contents.sections.base import Section
+from imio.smartweb.locales import SmartwebMessageFactory as _
 from zope.interface import implementer
+from zope import schema
 
 
 class ISectionGallery(ISection):
     """Marker interface and Dexterity Python Schema for SectionGallery"""
+
+    image_scale = schema.Choice(
+        title=_(u"Image scale"),
+        default=u"preview",
+        vocabulary="plone.app.vocabularies.ImagesScales",
+        required=True,
+    )
 
 
 @implementer(ISectionGallery)
--- src/imio/smartweb/core/contents/sections/links/content.py
+++ src/imio/smartweb/core/contents/sections/links/content.py
@@ -2,11 +2,20 @@
 
 from imio.smartweb.core.contents.sections.base import ISection
 from imio.smartweb.core.contents.sections.base import Section
+from imio.smartweb.locales import SmartwebMessageFactory as _
 from zope.interface import implementer
+from zope import schema
 
 
 class ISectionLinks(ISection):
     """Marker interface and Dexterity Python Schema for SectionFiles"""
+
+    image_scale = schema.Choice(
+        title=_(u"Image scale"),
+        default=u"tile",
+        vocabulary="plone.app.vocabularies.ImagesScales",
+        required=True,
+    )
 
 
 @implementer(ISectionLinks)
