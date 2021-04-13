e8c03ac4cf87f5f8aba7e6e6e2452e5a6168fd0a
Christophe Boulanger <christophe.boulanger@imio.be>
2021-04-13 15:42:52 +0200

    remove useless method

--- src/imio/smartweb/core/contents/sections/links/view.py
+++ src/imio/smartweb/core/contents/sections/links/view.py
@@ -1,12 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from zope.component import queryMultiAdapter
 from imio.smartweb.core.contents.sections.views import SectionView
 
 
 class LinksView(SectionView):
     """Links Section view"""
-
-    def get_thumb_scale_list(self):
-        view = queryMultiAdapter((self.context, self.request), name="listing_view")
-        return view.get_thumb_scale_list()
