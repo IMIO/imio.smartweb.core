aadf5ae5a8f5d8d588c9542ed416fa05bb1f6587
Christophe Boulanger <christophe.boulanger@imio.be>
2021-04-13 15:43:05 +0200

    Add footer tests

---
+++ src/imio/smartweb/core/tests/test_footer.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+
+from imio.smartweb.core.testing import IMIO_SMARTWEB_CORE_INTEGRATION_TESTING
+from imio.smartweb.core.viewlets.footer import FooterViewlet
+from imio.smartweb.core.viewlets.footer import SubsiteFooterViewlet
+from plone import api
+from plone.app.testing import TEST_USER_ID
+from plone.app.testing import setRoles
+from zope.component import getMultiAdapter
+import unittest
+
+
+class FooterIntegrationTest(unittest.TestCase):
+
+    layer = IMIO_SMARTWEB_CORE_INTEGRATION_TESTING
+
+    def setUp(self):
+        """Custom shared utility setup for tests"""
+        self.request = self.layer["request"]
+        self.portal = self.layer["portal"]
+        setRoles(self.portal, TEST_USER_ID, ["Manager"])
+        self.folder = api.content.create(
+            container=self.portal,
+            type="imio.smartweb.Folder",
+            title="Folder",
+            id="folder",
+        )
+
+    def test_add_footer_to_plonesite(self):
+        view = getMultiAdapter((self.portal, self.request), name="footer_settings")
+        self.assertTrue(view.available)
+        viewlet = FooterViewlet(self.portal, self.request, None, None)
+        viewlet.update()
+        self.assertFalse(viewlet.available())
+        view.add_footer()
+        footers = self.portal.listFolderContents(
+            contentFilter={"portal_type": "imio.smartweb.Footer"}
+        )
+        self.assertEquals(len(footers), 1)
+        self.assertFalse(view.available)
+        viewlet = FooterViewlet(self.portal, self.request, None, None)
+        viewlet.update()
+        self.assertTrue(viewlet.available())
+        self.assertEquals(viewlet.footer, footers[0])
+        api.content.delete(footers[0])
+        self.assertTrue(view.available)
+
+    def test_add_footer_to_subsite(self):
+        footer_view = getMultiAdapter((self.folder, self.request), name="footer_settings")
+        self.assertFalse(footer_view.available)
+        subsite_view = getMultiAdapter((self.folder, self.request), name="subsite_settings")
+        subsite_view.enable()
+        self.assertTrue(footer_view.available)
+        footer_view.add_footer()
+        footers = self.folder.listFolderContents(
+            contentFilter={"portal_type": "imio.smartweb.Footer"}
+        )
+        self.assertEquals(len(footers), 1)
+        self.assertFalse(footer_view.available)
+        api.content.delete(footers[0])
+        self.assertTrue(footer_view.available)
+
+    def test_add_footer_to_nested_subsite(self):
+        nested_folder = api.content.create(
+            container=self.folder,
+            type="imio.smartweb.Folder",
+            title="Nested folder",
+            id="nested_folder",
+        )
+        subsite_view = getMultiAdapter((self.folder, self.request), name="subsite_settings")
+        subsite_view.enable()
+        footer_view = getMultiAdapter((self.folder, self.request), name="footer_settings")
+        footer_view.add_footer()
+
+        nested_subsite_view = getMultiAdapter((nested_folder, self.request), name="subsite_settings")
+        nested_subsite_view.enable()
+        nested_footer_view = getMultiAdapter((nested_folder, self.request), name="footer_settings")
+        nested_footer_view.add_footer()
+
+        viewlet = SubsiteFooterViewlet(self.folder, self.request, None, None)
+        viewlet.update()
+        self.assertTrue(viewlet.available())
+        nested_viewlet = SubsiteFooterViewlet(nested_folder, self.request, None, None)
+        nested_viewlet.update()
+        self.assertTrue(nested_viewlet.available())
+        self.assertNotEquals(viewlet.footer, nested_viewlet.footer)
