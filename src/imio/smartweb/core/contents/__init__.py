dcd60e5ede0fa00a1d6f2bf565242a92cb21a27b
Laurent Lasudry <lasudry@gmail.com>
2021-04-20 12:45:33 +0200

    Avoid QA errors

--- src/imio/smartweb/core/contents/__init__.py
+++ src/imio/smartweb/core/contents/__init__.py
@@ -1,11 +1,11 @@
-from .pages.pages import IPages, IDefaultPages, Pages
-from .folder.content import IFolder, Folder
-from .pages.footer.content import IFooter, Footer
-from .pages.page.content import IPage, Page
-from .pages.procedure.content import IProcedure, Procedure
-from .sections.base import ISection, Section
-from .sections.files.content import ISectionFiles, SectionFiles
-from .sections.gallery.content import ISectionGallery, SectionGallery
-from .sections.links.content import ISectionLinks, SectionLinks
-from .sections.text.content import ISectionText, SectionText
-from .sections.video.content import ISectionVideo, SectionVideo
+from .pages.pages import IPages, IDefaultPages, Pages  # NOQA
+from .folder.content import IFolder, Folder  # NOQA
+from .pages.footer.content import IFooter, Footer  # NOQA
+from .pages.page.content import IPage, Page  # NOQA
+from .pages.procedure.content import IProcedure, Procedure  # NOQA
+from .sections.base import ISection, Section  # NOQA
+from .sections.files.content import ISectionFiles, SectionFiles  # NOQA
+from .sections.gallery.content import ISectionGallery, SectionGallery  # NOQA
+from .sections.links.content import ISectionLinks, SectionLinks  # NOQA
+from .sections.text.content import ISectionText, SectionText  # NOQA
+from .sections.video.content import ISectionVideo, SectionVideo  # NOQA
