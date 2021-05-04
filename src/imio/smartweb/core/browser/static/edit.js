468cedd9aabd87e80fb7a2cd22f881866f7b3d97
Laurent Lasudry <lasudry@gmail.com>
2021-05-04 14:36:14 +0200

    Add comment

--- src/imio/smartweb/core/browser/static/edit.js
+++ src/imio/smartweb/core/browser/static/edit.js
@@ -1,9 +1,12 @@
 jQuery(document).ready(function ($) {
 
+  // Hide / show editor's tools when clicking on "Preview" in Plone toolbar
+
   $("#contentview-preview a").click(function(e){
     $(".hide-in-preview").toggle("fast");
     e.preventDefault();
   });
+
 
   // Toggle on add / edit forms fieldsets
 
