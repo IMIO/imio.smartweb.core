e41697df6b87d9880c38783c4f59c68a12e7383f
Christophe Boulanger <christophe.boulanger@imio.be>
2021-05-04 14:10:54 +0200

    blacked file

--- src/imio/smartweb/core/viewlets/menu.py
+++ src/imio/smartweb/core/viewlets/menu.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 
-from plone.app.layout.viewlets.common import ContentViewsViewlet as ContentViewsViewletBase
+from plone.app.layout.viewlets.common import (
+    ContentViewsViewlet as ContentViewsViewletBase,
+)
 
 
 class ContentViewsViewlet(ContentViewsViewletBase):
     """Add preview action to primary actions to ensure ordering"""
+
     primary = ["folderContents", "edit", "view", "preview"]
