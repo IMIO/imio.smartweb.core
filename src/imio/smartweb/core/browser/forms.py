823935fad81dd063b42cd38dd6ccf6926d4c5035
Christophe Boulanger <christophe.boulanger@imio.be>
2021-06-07 12:18:49 +0200

    Add updateWidget to set new text description on description field.

--- src/imio/smartweb/core/browser/forms.py
+++ src/imio/smartweb/core/browser/forms.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from imio.smartweb.core.contents import IDefaultPages
+from imio.smartweb.locales import SmartwebMessageFactory as _
 from plone.dexterity.browser.add import DefaultAddForm
 from plone.dexterity.browser.add import DefaultAddView
 from plone.dexterity.browser.edit import DefaultEditForm
@@ -18,6 +19,12 @@ class CustomAddForm(DefaultAddForm):
         if "ILeadImageBehavior.image_caption" in self.fields:
             # We don't use leadimage caption anywhere
             self.fields["ILeadImageBehavior.image_caption"].mode = HIDDEN_MODE
+
+    def updateWidgets(self):
+        super(CustomAddForm, self).updateWidgets()
+        self.widgets["IBasic.description"].description = _(
+            u"Use **text** to set text in bold and *text* to set text in italic."
+        )
 
 
 class CustomAddView(DefaultAddView):
@@ -41,6 +48,12 @@ class CustomEditForm(DefaultEditForm):
                     group.fields[
                         "IExcludeFromNavigation.exclude_from_nav"
                     ].mode = DISPLAY_MODE
+
+    def updateWidgets(self):
+        super(CustomEditForm, self).updateWidgets()
+        self.widgets["IBasic.description"].description = _(
+            u"Use **text** to set text in bold and *text* to set text in italic."
+        )
 
 
 CustomEditView = layout.wrap_form(CustomEditForm)
