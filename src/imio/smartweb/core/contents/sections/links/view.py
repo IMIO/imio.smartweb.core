d070af4c77fecd6f086ec02921da5b3793b11c3f
Christophe Boulanger <christophe.boulanger@imio.be>
2021-06-08 15:45:36 +0200

    Move table and carousel templates as common_templates
    and add items method on links section view.

--- src/imio/smartweb/core/contents/sections/links/view.py
+++ src/imio/smartweb/core/contents/sections/links/view.py
@@ -5,3 +5,6 @@ from imio.smartweb.core.contents.sections.views import SectionView
 
 class LinksView(SectionView):
     """Links Section view"""
+
+    def items(self):
+        return self.context.listFolderContents()
