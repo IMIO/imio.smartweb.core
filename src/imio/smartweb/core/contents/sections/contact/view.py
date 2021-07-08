8440d06cb1ec51c72e83f18d025232f914647b99
Christophe Boulanger <christophe.boulanger@imio.be>
2021-07-08 16:08:52 +0200

    Add opening_informations method to contact view

--- src/imio/smartweb/core/contents/sections/contact/view.py
+++ src/imio/smartweb/core/contents/sections/contact/view.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 
+from datetime import date, datetime
 from imio.smartweb.core.contents.sections.views import SectionView
 from imio.smartweb.core.utils import get_directory_json
+from imio.smartweb.locales import SmartwebMessageFactory as _
 from plone import api
 
 
@@ -34,3 +36,48 @@ class ContactView(SectionView):
         if json_images is None or len(json_images.get("items", [])) == 0:
             return
         return json_images.get("items")
+
+    @property
+    def days(self):
+        return (
+            ("monday", _("Monday")),
+            ("tuesday", _("Tuesday")),
+            ("wednesday", _("Wednesday")),
+            ("thursday", _("Thursday")),
+            ("friday", _("Friday")),
+            ("saturday", _("Saturday")),
+            ("sunday", _("Sunday")),
+        )
+
+    def get_opening_informations(self):
+        contact = self.contact
+        if contact is None:
+            return
+        schedule = contact.get("schedule")
+        multi_schedule = contact.get("multi_schedule")
+        exceptional_closure = contact.get("exceptional_closure")
+
+        current_date = date.today()
+        # current_hour = datetime.now()
+        week_day = self.days[current_date.weekday()][0]
+        day_schedule = schedule[week_day]
+        for schedule_info in multi_schedule:
+            if schedule_info.get("dates") == []:
+                continue
+            min_date = datetime.strptime(schedule_info.get("dates"), "%Y-%m-%d").date()
+            max_date = datetime.strptime(schedule_info.get("dates"), "%Y-%m-%d").date()
+            if not (min_date < current_date < max_date):
+                continue
+            day_schedule = schedule_info.get("schedule")[week_day]
+
+        for exceptional_closure_info in exceptional_closure:
+            exceptional_closure_date = datetime.strptime(
+                exceptional_closure_info.get("date"), "%Y-%m-%d"
+            ).date()
+            if not current_date == exceptional_closure_date:
+                continue
+            for k, v in day_schedule.items():
+                if k == "comment":
+                    day_schedule[k] = exceptional_closure_info.get("title")
+                day_schedule[k] = ""
+        return day_schedule
