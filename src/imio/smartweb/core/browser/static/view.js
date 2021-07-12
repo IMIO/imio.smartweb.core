020e91cc266b809046e6db7c20042af0b02ca093
Laurent Lasudry <lasudry@gmail.com>
2021-07-12 14:43:11 +0200

    Fix comment

--- src/imio/smartweb/core/browser/static/view.js
+++ src/imio/smartweb/core/browser/static/view.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function ($) {
 
-  // Hide / show editor's tools when clicking on "Preview" in Plone toolbar
+  // Show full schedule table when clicking on today's schedule
 
   $(".opening_informations").click(function(e){
     $(this).siblings(".table_schedule").toggle("fast");
