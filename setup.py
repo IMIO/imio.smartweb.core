74542cea804189e1a5b298c8f7df22b704211903
Laurent Lasudry <lasudry@gmail.com>
2021-07-16 15:23:48 +0200

    Back to development: 1.0a10

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = "\n\n".join(
 
 setup(
     name="imio.smartweb.core",
-    version='1.0a9',
+    version='1.0a10.dev0',
     description="Core product for iMio websites",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
