04f44ef4cc0735583252a2af7ee08ad1139e05d8
Christophe Boulanger <christophe.boulanger@imio.be>
2021-09-10 11:32:09 +0200

    Set IImioSmartwebCoreLayer more specific

--- src/imio/smartweb/core/interfaces.py
+++ src/imio/smartweb/core/interfaces.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
-
+from collective.anysurfer.interfaces import ILayerSpecific
 from imio.smartweb.common.interfaces import IImioSmartwebCommonLayer
 from plone.app.contenttypes.interfaces import IPloneAppContenttypesLayer
 
 
-class IImioSmartwebCoreLayer(IImioSmartwebCommonLayer, IPloneAppContenttypesLayer):
+class IImioSmartwebCoreLayer(
+    IImioSmartwebCommonLayer, IPloneAppContenttypesLayer, ILayerSpecific
+):
     """Marker interface that defines a browser layer."""
