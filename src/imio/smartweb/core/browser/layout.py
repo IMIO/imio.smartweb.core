b2744a372f1b9848a02c4d6fd7d5b2ef564b4808
Christophe Boulanger <christophe.boulanger@imio.be>
2021-09-10 11:32:42 +0200

    Add new minisite and subsite body class

--- src/imio/smartweb/core/browser/layout.py
+++ src/imio/smartweb/core/browser/layout.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
-
+from collective.anysurfer.layout import LayoutPolicy as AnysurferLayoutPolicy
+from imio.smartweb.core.behaviors.minisite import IImioSmartwebMinisite
 from imio.smartweb.core.viewlets.banner import BannerViewlet
+from imio.smartweb.core.viewlets.subsite import BaseSubsiteViewlet
+from plone import api
 from plone.app.layout.globals import layout as base
 
 
-class LayoutPolicy(base.LayoutPolicy):
+class LayoutPolicy(AnysurferLayoutPolicy):
     """
     Enhanced layout policy
     """
@@ -13,6 +16,8 @@ class LayoutPolicy(base.LayoutPolicy):
         """
         Extend Plone to add a CSS class on <body> based on :
          1. banner image
+         2. minisite
+         3. subsite
         """
         context = self.context
         request = self.request
@@ -20,8 +25,19 @@ class LayoutPolicy(base.LayoutPolicy):
         # Get default body classes
         body_class = base.LayoutPolicy.bodyClass(self, template, view)
 
-        # Add bloc-banner class if there is a banner on the page
+        # 1. Add bloc-banner class if there is a banner on the page
         banner_viewlet = BannerViewlet(context, request, view)
         if banner_viewlet.available() and not banner_viewlet.is_banner_locally_hidden:
             body_class += " bloc-banner"
+
+        # 2. Add is-in-minisite class to body if we are in a minisite
+        root = api.portal.get_navigation_root(context)
+        if IImioSmartwebMinisite.providedBy(root):
+            body_class += " is-in-minisite"
+
+        # 3. Add is-in-subsite class to body if we are in a subsite
+        subsite_viewlet = BaseSubsiteViewlet(context, request, view)
+        if subsite_viewlet.available():
+            body_class += " is-in-subsite"
+
         return body_class
