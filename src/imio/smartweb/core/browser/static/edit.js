5aa78fbe29d3eb630ee626f0af0f55b3de9c39d2
Laurent Lasudry <lasudry@gmail.com>
2021-09-15 15:09:28 +0200

    Hide more messages when previewing content

--- src/imio/smartweb/core/browser/static/edit.js
+++ src/imio/smartweb/core/browser/static/edit.js
@@ -1,9 +1,9 @@
 jQuery(document).ready(function ($) {
 
-  // Hide / show editor's tools when clicking on "Preview" in Plone toolbar
+  // Hide / show editor's tools & messages when clicking on "Preview" in Plone toolbar
 
   $("#contentview-preview a").click(function(e){
-    $(".hide-in-preview").toggle("fast");
+    $(".hide-in-preview, #section-byline, #global_statusmessage").toggle("fast");
     e.preventDefault();
   });
 
