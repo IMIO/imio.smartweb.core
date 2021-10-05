b4b5359f7851ba9edeed4f35b007c6cde1e60846
Laurent Lasudry <lasudry@gmail.com>
2021-10-05 13:21:06 +0200

    Remove italic from description help text
    It is not supported anymore

--- src/imio/smartweb/core/contents/sections/links/content.py
+++ src/imio/smartweb/core/contents/sections/links/content.py
@@ -14,7 +14,7 @@ class ISectionLinks(ISection):
     description = schema.Text(
         title=_(u"Description"),
         description=_(
-            u"Use **text** to set text in bold and *text* to set text in italic."
+            u"Use **text** to set text in bold."
         ),
         required=False,
     )
