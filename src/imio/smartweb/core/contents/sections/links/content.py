6b2557849146f9e9e73cc4076b387e3971c45f87
Laurent Lasudry <lasudry@gmail.com>
2021-10-05 15:34:28 +0200

    Limit description field to X characters

--- src/imio/smartweb/core/contents/sections/links/content.py
+++ src/imio/smartweb/core/contents/sections/links/content.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from imio.smartweb.common.config import DESCRIPTION_MAX_LENGTH
 from imio.smartweb.core.contents.sections.base import ISection
 from imio.smartweb.core.contents.sections.base import Section
 from imio.smartweb.locales import SmartwebMessageFactory as _
@@ -13,7 +14,11 @@ class ISectionLinks(ISection):
 
     description = schema.Text(
         title=_(u"Description"),
-        description=_(u"Use **text** to set text in bold."),
+        description=_(
+            u"Use **text** to set text in bold. Limited to ${max} characters.",
+            mapping={u"max": DESCRIPTION_MAX_LENGTH},
+        ),
+        max_length=DESCRIPTION_MAX_LENGTH,
         required=False,
     )
 
