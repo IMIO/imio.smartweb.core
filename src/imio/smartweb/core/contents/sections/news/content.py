7143b666c2f7a3ab7ab458cdc1ba95af90e2e946
Laurent Lasudry <lasudry@gmail.com>
2021-10-06 13:27:44 +0200

    Cleanup

--- src/imio/smartweb/core/contents/sections/news/content.py
+++ src/imio/smartweb/core/contents/sections/news/content.py
@@ -5,21 +5,12 @@ from imio.smartweb.core.contents.sections.base import Section
 from imio.smartweb.locales import SmartwebMessageFactory as _
 from plone.app.z3cform.widget import SelectFieldWidget
 from plone.autoform import directives
-from z3c.form.browser.checkbox import CheckBoxFieldWidget
 from zope import schema
 from zope.interface import implementer
 
 
 class ISectionNews(ISection):
     """Marker interface and Dexterity Python Schema for SectionNews"""
-
-    description = schema.Text(
-        title=_(u"Description"),
-        description=_(
-            u"Use **text** to set text in bold and *text* to set text in italic."
-        ),
-        required=False,
-    )
 
     directives.widget(related_news=SelectFieldWidget)
     related_news = schema.Choice(
