3bf5aa47696aaab27e82f6a5301a3f7d5cc27e3b
Christophe Boulanger <christophe.boulanger@imio.be>
2021-10-13 09:53:39 +0200

    Add image_scale field to events section

--- src/imio/smartweb/core/contents/sections/events/content.py
+++ src/imio/smartweb/core/contents/sections/events/content.py
@@ -5,6 +5,7 @@ from imio.smartweb.core.contents.sections.base import Section
 from imio.smartweb.locales import SmartwebMessageFactory as _
 from plone.app.z3cform.widget import SelectFieldWidget
 from plone.autoform import directives
+from plone.supermodel import model
 from zope import schema
 from zope.interface import implementer
 
@@ -12,14 +13,14 @@ from zope.interface import implementer
 class ISectionEvents(ISection):
     """Marker interface and Dexterity Python Schema for SectionEvents"""
 
-    directives.widget(related_Events=SelectFieldWidget)
-    related_Events = schema.Choice(
+    directives.widget(related_events=SelectFieldWidget)
+    related_events = schema.Choice(
         title=_(u"Related Events"),
         description=_(
             u"Select a Events. If you can't find the Events you want, make sure "
             u"""it exists in the directory and that its "state" is published."""
         ),
-        source="imio.smartweb.vocabulary.RemoteEventsFolders",
+        source="imio.smartweb.vocabulary.RemoteAgendas",
         required=True,
     )
 
@@ -29,6 +30,14 @@ class ISectionEvents(ISection):
 
     nb_results_by_batch = schema.Int(
         title=_(u"Number of items by batch"), default=3, required=True
+    )
+
+    model.fieldset("layout", fields=["image_scale"])
+    image_scale = schema.Choice(
+        title=_(u"Image scale for news"),
+        default=u"tile",
+        vocabulary="plone.app.vocabularies.ImagesScales",
+        required=True,
     )
 
 
