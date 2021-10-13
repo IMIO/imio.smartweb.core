93ee653f58efb86280d7967addc292fd65316a74
Christophe Boulanger <christophe.boulanger@imio.be>
2021-10-13 09:56:57 +0200

    Add plone.restapi[test] for rest test

--- setup.py
+++ setup.py
@@ -73,6 +73,7 @@ setup(
             "plone.testing>=5.0.0",
             "plone.app.contenttypes",
             "plone.app.robotframework[debug]",
+            "plone.restapi[test]",
             "requests-mock",
             "beautifulsoup4",
             "freezegun",
