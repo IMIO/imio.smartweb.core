d174bb36981a61b9956b46dc541db30b86602039
Martin Peeters <martin.peeters@affinitic.be>
2021-10-15 13:05:19 +0200

    Adapt `@search` endpoint
    - Transform urls for contents from external sources
    - Filter results by content that are displayed by react views

--- src/imio/smartweb/core/contents/rest/search/endpoint.py
+++ src/imio/smartweb/core/contents/rest/search/endpoint.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 
+from plone import api
+from plone.memoize import ram
 from plone.restapi.search.handler import SearchHandler
 from plone.restapi.search.utils import unflatten_dotted_dict
 from plone.restapi.services import Service
-from plone import api
+from time import time
 
 import pkg_resources
 
@@ -13,6 +15,64 @@ try:
     from collective.solr.search import Search
 except pkg_resources.DistributionNotFound:
     HAS_SOLR = False
+
+
+@ram.cache(lambda *args: time() // (5 * 60))
+def get_news_views():
+    with api.env.adopt_user(username="admin"):
+        brains = api.content.find(
+            context=api.portal.get(), portal_type="imio.smartweb.NewsView"
+        )
+        # Can be improved by using index
+        return {b.getObject().selected_news_folder: b.getURL() for b in brains}
+
+
+@ram.cache(lambda *args: time() // (5 * 60))
+def get_events_views():
+    with api.env.adopt_user(username="admin"):
+        brains = api.content.find(
+            context=api.portal.get(), portal_type="imio.smartweb.EventsView"
+        )
+        # Can be improved by using index
+        return {b.getObject().selected_agenda: b.getURL() for b in brains}
+
+
+@ram.cache(lambda *args: time() // (5 * 60))
+def get_directory_views():
+    with api.env.adopt_user(username="admin"):
+        brains = api.content.find(
+            context=api.portal.get(), portal_type="imio.smartweb.DirectoryView"
+        )
+        # Can be improved by using index
+        return {b.getObject().selected_entity: b.getURL() for b in brains}
+
+
+def get_default_view_url(view_type):
+    record = api.portal.get_registry_record(
+        "smartweb.default_{0}_view".format(view_type), default=None
+    )
+    if not record:
+        return ""
+    with api.env.adopt_user(username="admin"):
+        return api.content.get(UID=record).absolute_url()
+
+
+def get_views_mapping():
+    news = get_news_views()
+    news["default"] = get_default_view_url("news")
+
+    events = get_events_views()
+    events["default"] = get_default_view_url("events")
+
+    directory = get_directory_views()
+    directory["default"] = get_default_view_url("directory")
+
+    mapping = {
+        "imio.news.NewsItem": news,
+        "imio.events.Event": events,
+        "imio.directory.Contact": directory,
+    }
+    return mapping
 
 
 class SearchGet(Service):
@@ -28,30 +88,72 @@ class ExtendedSearchHandler(SearchHandler):
             parameters = self._core_query(query["_core"])
             del query["_core"]
             if parameters:
+                if "metadata_fields" in query:
+                    parameters["metadata_fields"] = self._update_metadata_fields(
+                        query["metadata_fields"],
+                        parameters["metadata_fields"],
+                    )
+                self.request.form["metadata_fields"] = parameters["metadata_fields"]
                 query.update(parameters)
-        return super(ExtendedSearchHandler, self).search(query)
+        result = super(ExtendedSearchHandler, self).search(query)
+        if "core" in query:
+            return self._adapt_result(result)
+        return result
+
+    def _adapt_result(self, result):
+        """Transform result"""
+        mapping = get_views_mapping()
+        result["items"] = [self._adapt_result_url(i, mapping) for i in result["items"]]
+        return result
+
+    def _adapt_result_url(self, item, mapping):
+        """Ensure that url to external objects are adapted based on react views"""
+        if item["@type"] not in mapping:
+            return item
+        type_mapping = mapping[item["@type"]]
+        base_url = type_mapping.get(item["container_uid"], type_mapping["default"])
+        item["_url"] = "{base}/{item_id}/{item_uid}".format(
+            base=base_url,
+            item_id=item["id"],
+            item_uid=item["UID"],
+        )
+        return item
+
+    def _update_metadata_fields(self, original, new):
+        if isinstance(original, str):
+            original = [original]
+        return [*original, *[e for e in new if e not in original]]
 
     def _core_query(self, core):
+        """Return core specific query parameters"""
+        mapping = get_views_mapping()
         parameters = {
             "news": {
                 "portal_type": ["imio.news.NewsItem"],
+                "metadata_fields": ["id", "UID", "container_uid", "has_leadimage"],
+                "selected_news_folders": [
+                    k for k in mapping["imio.news.NewsItem"].keys() if k != "default"
+                ],
             },
             "events": {
                 "portal_type": ["imio.events.Event"],
+                "metadata_fields": ["id", "UID", "container_uid", "has_leadimage"],
+                "selected_agendas": [
+                    k for k in mapping["imio.events.Event"].keys() if k != "default"
+                ],
             },
             "directory": {
                 "portal_type": ["imio.directory.Contact"],
+                "metadata_fields": ["id", "UID", "container_uid", "has_leadimage"],
+                "selected_entities": [
+                    k
+                    for k in mapping["imio.directory.Contact"].keys()
+                    if k != "default"
+                ],
             },
         }.get(core)
 
         if parameters:
-            if HAS_SOLR is True:
-                entity_uid = api.portal.get_registry_record(
-                    "smartweb.{0}_entity_uid".format(core)
-                )
-                results = Search().search('UID:"{0}"'.format(entity_uid), core=core)
-                if results:
-                    parameters["path"] = results[0].path_string
             parameters["core"] = api.portal.get_registry_record(
                 "smartweb.{0}_solr_core".format(core)
             )
