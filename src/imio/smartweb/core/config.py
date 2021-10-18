c18bfa1302490e5cc70a9d089997686bf503247f
Laurent Lasudry <lasudry@gmail.com>
2021-10-18 13:48:53 +0200

    Change events production URL

--- src/imio/smartweb/core/config.py
+++ src/imio/smartweb/core/config.py
@@ -1,3 +1,3 @@
 DIRECTORY_URL = "https://annuaire.enwallonie.be"
-EVENTS_URL = "https://events.staging.imio.be"
+EVENTS_URL = "https://agenda.enwallonie.be"
 NEWS_URL = "https://actualites.enwallonie.be"
