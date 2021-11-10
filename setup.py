d3d889cb32df7adfb22edc18cf234a1a918a22b9
Laurent Lasudry <lasudry@gmail.com>
2021-11-10 12:10:53 +0100

    Add development status classifier
    This will also fix badge

--- setup.py
+++ setup.py
@@ -21,6 +21,7 @@ setup(
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
     classifiers=[
+        "Development Status :: 3 - Alpha",
         "Environment :: Web Environment",
         "Framework :: Plone",
         "Framework :: Plone :: Addon",
