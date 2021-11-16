a2b9a8030f708a3b2128c13219ad3c0fb72d8b7a
Laurent Lasudry <lasudry@gmail.com>
2021-11-16 16:23:23 +0100

    Allow to specify embedded width/height for videos

--- src/imio/smartweb/core/contents/sections/video/views.py
+++ src/imio/smartweb/core/contents/sections/video/views.py
@@ -5,6 +5,6 @@ from imio.smartweb.core.contents.sections.views import SectionView
 
 
 class VideoView(SectionView):
-    def get_embed_video(self):
-        embedder = Embedder(width=800, height=600)
+    def get_embed_video(self, width=800, height=600):
+        embedder = Embedder(width=width, height=height)
         return embedder(self.context.video_url, params=dict(autoplay=False))
