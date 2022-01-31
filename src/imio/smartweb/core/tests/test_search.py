9e0b23de8915cb32d8fb06f883a185c161004d46
Christophe Boulanger <christophe.boulanger@imio.be>
2022-01-31 15:55:21 +0100

    Fix search tests

--- src/imio/smartweb/core/tests/test_search.py
+++ src/imio/smartweb/core/tests/test_search.py
@@ -1,12 +1,11 @@
 # -*- coding: utf-8 -*-
 
-from eea.facetednavigation.layout.interfaces import IFacetedLayout
 from imio.smartweb.core.testing import IMIO_SMARTWEB_CORE_FUNCTIONAL_TESTING
 from imio.smartweb.core.testing import ImioSmartwebTestCase
 from plone import api
 from plone.app.testing import TEST_USER_ID
 from plone.app.testing import setRoles
-from zope.component import getMultiAdapter
+from plone.app.textfield.value import RichTextValue
 from zope.component import queryMultiAdapter
 
 
@@ -19,11 +18,40 @@ class TestFaceted(ImioSmartwebTestCase):
         self.request = self.layer["request"]
         self.portal = self.layer["portal"]
         setRoles(self.portal, TEST_USER_ID, ["Manager"])
+        self.folder = api.content.create(
+            container=self.portal,
+            type="imio.smartweb.Folder",
+            id="folder",
+        )
+        api.content.transition(self.folder, "publish")
 
     def test_search(self):
+        self.request.form["SearchableText"] = "kamoulox"
         search = queryMultiAdapter((self.portal, self.request), name="search")
-        self.assertEqual(search.results().pagesize, 10)
-        self.assertEqual(search.results().size, 0)
+        self.assertEqual(len(search.results()), 0)
 
-        search = queryMultiAdapter((self.portal, self.request), name="search")
-        self.assertEqual(search.results_news().size, 0)
+        page = api.content.create(
+            container=self.folder,
+            type="imio.smartweb.Page",
+            title="kamoulox",
+        )
+
+        self.assertEqual(len(search.results()), 1)
+
+        section_text = api.content.create(
+            container=page,
+            type="imio.smartweb.SectionText",
+            title="kamoulox",
+        )
+        section_text.text = RichTextValue("<p>kamoulox</p>", "text/html", "text/html")
+        section_text.reindexObject()
+
+        # Some sections (like SectionText) are not searchable
+        self.assertEqual(len(search.results()), 1)
+
+        api.content.create(
+            container=self.portal,
+            type="imio.smartweb.PortalPage",
+            title="kamoulox",
+        )
+        self.assertEqual(len(search.results()), 2)
