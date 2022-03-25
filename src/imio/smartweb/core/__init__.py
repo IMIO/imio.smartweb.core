694b21e586a17e35d3dc98bfab2f76e5984e565f
Laurent Lasudry <lasudry@gmail.com>
2022-03-25 12:32:23 +0100

    Move code to imio.smartweb.policy

--- src/imio/smartweb/core/__init__.py
+++ src/imio/smartweb/core/__init__.py
@@ -1,10 +1,1 @@
 # -*- coding: utf-8 -*-
-"""Init and utils."""
-
-# collective.themefragments
-from AccessControl import allow_module, allow_class, allow_type
-from requests.models import Response
-
-allow_module("requests")
-allow_class(Response)
-allow_type(type("requests.models.Response"))
