22d9c8ca6a4f49b59672ff89ccac882a338b6716
Laurent Lasudry <lasudry@gmail.com>
2022-06-19 09:37:59 +0200

    Add tests for map markers popups

--- src/imio/smartweb/core/tests/test_faceted.py
+++ src/imio/smartweb/core/tests/test_faceted.py
@@ -1,12 +1,17 @@
 # -*- coding: utf-8 -*-
 
 from eea.facetednavigation.layout.interfaces import IFacetedLayout
+from imio.smartweb.core.interfaces import IImioSmartwebCoreLayer
 from imio.smartweb.core.testing import IMIO_SMARTWEB_CORE_FUNCTIONAL_TESTING
 from imio.smartweb.core.testing import ImioSmartwebTestCase
+from imio.smartweb.core.tests.utils import make_named_image
 from plone import api
 from plone.app.testing import TEST_USER_ID
 from plone.app.testing import setRoles
+from plone.namedfile.file import NamedBlobImage
+from plone.uuid.interfaces import IUUID
 from zope.component import getMultiAdapter
+from zope.interface import alsoProvides
 
 
 class TestFaceted(ImioSmartwebTestCase):
@@ -47,3 +52,30 @@ class TestFaceted(ImioSmartwebTestCase):
         view = getMultiAdapter((self.collection, self.request), name="view")
         body_class = layout_view.bodyClass(template, view)
         self.assertNotIn("faceted-summary-view-with-images", body_class)
+
+    def test_map_popup(self):
+        alsoProvides(self.request, IImioSmartwebCoreLayer)
+        collection = api.content.create(
+            container=self.portal,
+            type="Collection",
+            title="My collection",
+            description="My **description**",
+        )
+        uuid = IUUID(collection)
+        brain = api.content.find(UID=uuid)[0]
+        popup_view = getMultiAdapter(
+            (collection, self.request), name="faceted-map-geojson-popup"
+        )
+        popup = popup_view.popup(brain)
+        self.assertNotIn("<img", popup)
+        self.assertIn('href="http://nohost/plone/my-collection"', popup)
+        self.assertIn("My collection", popup)
+        self.assertIn("My description", popup)
+
+        collection.image = NamedBlobImage(**make_named_image())
+        collection.reindexObject()
+        brain = api.content.find(UID=uuid)[0]
+        popup = popup_view.popup(brain)
+        self.assertIn(
+            '<img src="http://nohost/plone/my-collection/@@images/image/mini"', popup
+        )
