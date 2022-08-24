7c0da6e9eba79dc215e0449f297036227a8cd835
Christophe Boulanger <christophe.boulanger@imio.be>
2022-08-24 16:11:18 +0200

    WEB-3741 : Fix directory rest view endpoints @search , @search-filters

--- src/imio/smartweb/core/contents/rest/directory/endpoint.py
+++ src/imio/smartweb/core/contents/rest/directory/endpoint.py
@@ -36,11 +36,53 @@ class BaseDirectoryEndpoint(BaseEndpoint):
 class DirectoryEndpoint(BaseDirectoryEndpoint):
     remote_endpoint = "@search"
 
+    def __call__(self):
+        res = super(DirectoryEndpoint, self).__call__()
+        if res == []:
+            return res
+        filtered_items = []
+        selected_contact_category_token = self.request.form.get(
+            "taxonomy_contact_category"
+        )
+        if self.request.form is None or selected_contact_category_token is None:
+            return res
+        for item in res.get("items"):
+            selected_contact_category_title = [
+                tcc.get("title")
+                for tcc in item.get("taxonomy_contact_category")
+                if tcc.get("token") == selected_contact_category_token
+            ]
+            if selected_contact_category_title and selected_contact_category_title[
+                0
+            ] in [tcc.get("title") for tcc in item.get("taxonomy_contact_category")]:
+                filtered_items.append(item)
+        res["items"] = filtered_items
+        res["items_total"] = len(filtered_items)
+        return res
+
 
 @implementer(IExpandableElement)
 @adapter(Interface, Interface)
 class DirectoryFiltersEndpoint(BaseDirectoryEndpoint):
     remote_endpoint = "@search-filters"
+
+    def __call__(self):
+        res = super(DirectoryFiltersEndpoint, self).__call__()
+        if res == []:
+            return res
+        filtered_categories_title = []
+        for selected_category_token in self.context.selected_categories:
+            for category in res.get("taxonomy_contact_category"):
+                if category.get("token") == selected_category_token:
+                    filtered_categories_title.append(category.get("title"))
+
+        filtered_categories = []
+        for title in filtered_categories_title:
+            for category in res.get("taxonomy_contact_category"):
+                if title in category.get("title"):
+                    filtered_categories.append(category)
+        res["taxonomy_contact_category"] = filtered_categories
+        return res
 
 
 class DirectoryEndpointGet(BaseService):
