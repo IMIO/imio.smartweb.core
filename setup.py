26638fe61e24cabfde7c08e41b0b85dc51a20296
Christophe Boulanger <christophe.boulanger@imio.be>
2022-09-08 10:34:55 +0200

    Back to development: 1.0.20

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = "\n\n".join(
 
 setup(
     name="imio.smartweb.core",
-    version="1.0.19",
+    version="1.0.20.dev0",
     description="Core product for iMio websites",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
