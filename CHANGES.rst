7efcece12f1271494d8faccded979f310bbb4269
Laurent Lasudry <lasudry@gmail.com>
2023-01-12 10:27:06 +0100

    Preparing release 1.1.1

--- CHANGES.rst
+++ CHANGES.rst
@@ -2,7 +2,7 @@ Changelog
 =========
 
 
-1.1.1 (unreleased)
+1.1.1 (2023-01-12)
 ------------------
 
 - Use generated image scale urls to increase image caching
--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = "\n\n".join(
 
 setup(
     name="imio.smartweb.core",
-    version="1.1.1.dev0",
+    version="1.1.1",
     description="Core product for iMio websites",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
