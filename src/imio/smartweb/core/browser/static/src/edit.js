18475a5d09f7d9c45d354b15aed71b98197cc3c3
Christophe Boulanger <christophe.boulanger@imio.be>
2023-01-25 13:27:41 +0100

    Add authentic sources menu in toolbar

--- src/imio/smartweb/core/browser/static/src/edit.js
+++ src/imio/smartweb/core/browser/static/src/edit.js
@@ -2,10 +2,14 @@ import './edit.less'
 jQuery(document).ready(function ($) {
 
   // Hide / show editor's tools & messages when clicking on "Preview" in Plone toolbar
-
   $("#contentview-preview a").click(function(e){
     $(".hide-in-preview, #section-byline, #global_statusmessage").toggle("fast");
     e.preventDefault();
   });
 
+  // Move authentic sources menu just before user/personaltools-menulink in Plone toolbar
+  var auth_sources = $("#plone-authentic-sources-menu").wrap("<ul class='plonetoolbar-authentic-sources-menu'>").parent();
+  $(".personaltools-wrapper").prepend(auth_sources);
+
 });
+
