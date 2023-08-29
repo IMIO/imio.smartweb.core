1ee0c5a23f56e4652f03b2cf64a3133c6ef976db
cboulanger <christophe.boulanger@imio.be>
2023-08-29 12:51:55 +0200

    Back to development: 1.1.22

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = "\n\n".join(
 
 setup(
     name="imio.smartweb.core",
-    version="1.1.21",
+    version="1.1.22.dev0",
     description="Core product for iMio websites",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
