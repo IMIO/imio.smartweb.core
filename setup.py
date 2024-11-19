f434ae7a253a17b117f30f8535fcbb7f1af9d9ec
boulch <christophe.boulanger@imio.be>
2024-11-19 15:46:39 +0100

    Back to development: 1.2.76

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = "\n\n".join(
 
 setup(
     name="imio.smartweb.core",
-    version="1.2.75",
+    version="1.2.76.dev0",
     description="Core product for iMio websites",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
