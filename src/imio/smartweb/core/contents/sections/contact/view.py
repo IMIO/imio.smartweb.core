23558e63c6bb1def1d42a2c486b4f7a17631fa4f
Laurent Lasudry <lasudry@gmail.com>
2024-11-19 13:47:46 +0100

    Avoid hash differences after WEBBDC-1265 changes
    We construct the same JSON data as before, ensuring the cached values (hash) do not change

--- src/imio/smartweb/core/contents/sections/contact/view.py
+++ src/imio/smartweb/core/contents/sections/contact/view.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 
+from imio.smartweb.core.config import DIRECTORY_URL
 from imio.smartweb.core.contents.sections.contact.utils import ContactProperties
 from imio.smartweb.core.contents.sections.views import HashableJsonSectionView
 from imio.smartweb.core.utils import batch_results
+from plone import api
 from zope.component import queryMultiAdapter
 
 
@@ -19,7 +21,7 @@ class ContactView(HashableJsonSectionView):
             return []
 
         container_contacts = self.json_data.get("items")
-        results = [
+        results_items = [
             contact
             for contact in container_contacts
             if contact["UID"] in self.context.related_contacts
@@ -27,12 +29,21 @@ class ContactView(HashableJsonSectionView):
         index_map = {
             value: index for index, value in enumerate(self.context.related_contacts)
         }
-        results = sorted(results, key=lambda x: index_map[x["UID"]])
-        # TODO there is for sure a better way than storing json_data that
-        # is not really JSON
-        self.json_data = results
+        results_items = sorted(results_items, key=lambda x: index_map[x["UID"]])
+
+        # construct JSON data as before WEBBDC-1265 to avoid hash differences
+        uids = "&UID=".join(self.context.related_contacts)
+        url = "{}/@search?UID={}&fullobjects=1".format(DIRECTORY_URL, uids)
+        current_lang = api.portal.get_current_language()[:2]
+        if current_lang != "fr":
+            url = f"{url}&translated_in_{current_lang}=1"
+        self.json_data = {
+            "@id": url,
+            "items": results_items,
+            "items_total": len(results_items),
+        }
         self.refresh_modification_date()
-        return batch_results(results, self.context.nb_contact_by_line)
+        return batch_results(results_items, self.context.nb_contact_by_line)
 
     def get_contact_properties(self, json_dict):
         return ContactProperties(json_dict, self.context)
