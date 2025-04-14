795087b914e86782f60e1281c95908c4e8be4435
thomlamb <lambertthomas10@gmail.com>
2025-04-14 15:54:04 +0200

    Change add Event icon

--- src/imio/smartweb/core/webcomponents/src/components/Events/EventContent/EventContent.jsx
+++ src/imio/smartweb/core/webcomponents/src/components/Events/EventContent/EventContent.jsx
@@ -121,6 +121,7 @@ const ContactContent = ({ queryUrl, onChange, onlyPastEvents, contextAuthenticat
     const closeSchedul = () => {
         setSchedulVisibility(false);
     };
+    console.log(recurence);
     return isLoading ? (
         <div className="lds-roller-container">
             <Translate text="Chargement..." />
@@ -153,7 +154,7 @@ const ContactContent = ({ queryUrl, onChange, onlyPastEvents, contextAuthenticat
                         width="16"
                         height="16"
                         fill="currentColor"
-                        class="bi bi-pencil-square"
+                        className="bi bi-pencil-square"
                         viewBox="0 0 16 16"
                     >
                         <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
@@ -324,7 +325,7 @@ const ContactContent = ({ queryUrl, onChange, onlyPastEvents, contextAuthenticat
                                             )}
                                             <span className="recurence-schedul-more">
                                                 Prochaines dates
-                                                <i class="bi bi-arrow-down-short"></i>
+                                                <i className="bi bi-arrow-down-short"></i>
                                             </span>
                                         </p>
                                         <div
