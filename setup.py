9ccec8ddce6afbf29514cf1241c4671679008af4
Rémi Dubois <100987383+remdub@users.noreply.github.com>
2025-05-06 14:00:25 +0200

    feat: remove ipdb from test dependencies

--- setup.py
+++ setup.py
@@ -86,7 +86,6 @@ setup(
             # Plone KGS does not use this version, because it would break
             # Remove if your package shall be part of coredev.
             # plone_coredev tests as of 2016-04-01.
-            "ipdb",
             "plone.testing>=5.0.0",
             "plone.app.contenttypes",
             "plone.app.robotframework[debug]",
