f404db7e408d82392475d8fc1e5332b171505552
thomlamb <lambertthomas10@gmail.com>
2025-05-20 15:27:10 +0200

    Back to development: 1.4.4

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = "\n\n".join(
 
 setup(
     name="imio.smartweb.core",
-    version="1.4.3",
+    version="1.4.4.dev0",
     description="Core product for iMio websites",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
