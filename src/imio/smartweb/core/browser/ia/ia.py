b540c361da9809476e115d168627d1714b32d826
boulch <christophe.boulanger@imio.be>
2025-10-02 09:14:29 +0200

    Use IPA environment url instead of hard coding url + clean useless code

--- src/imio/smartweb/core/browser/ia/ia.py
+++ src/imio/smartweb/core/browser/ia/ia.py
@@ -1,7 +1,5 @@
-# browser/slugify.py
+from imio.smartweb.common.config import IPA_URL
 from Products.Five import BrowserView
-from plone.i18n.normalizer.interfaces import IIDNormalizer
-from zope.component import getUtility
 
 import json
 import requests
@@ -26,7 +24,7 @@ class ProcessSuggestedTitlesView(BrowserView):
             "input": current_html,
             "expansion_target": 50,
         }
-        url = "http://127.0.0.1:8000/common/suggest-titles"
+        url = f"{IPA_URL}/suggest-titles"
         response = requests.post(url, headers=self.headers, json=payload)
         if response.status_code != 200:
             return current_html
@@ -34,12 +32,3 @@ class ProcessSuggestedTitlesView(BrowserView):
         if not data:
             return current_html
         return json.dumps(data)
-
-        # titles = data.get("suggested_titles", [])
-        # new_html = "<ul>{}</ul>".format("".join(f"<li>{t}</li>" for t in titles))
-        # return json.dumps({"html": f"{new_html}<br />{current_html}"})
-        # text = self.request.form.get("text", "")
-        # normalizer = getUtility(IIDNormalizer)
-        # value = normalizer.normalize(text)
-        # self.request.response.setHeader("Content-Type", "application/json")
-        # return json.dumps({"value": value})
