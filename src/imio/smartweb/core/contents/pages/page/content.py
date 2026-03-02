f8bff5385045b6864405ee8c258f614cd3bc0449
thomlamb <lambertthomas10@gmail.com>
2026-03-02 15:56:19 +0100

    Add option to align title and css

--- src/imio/smartweb/core/contents/pages/page/content.py
+++ src/imio/smartweb/core/contents/pages/page/content.py
@@ -4,10 +4,19 @@ from collective.instancebehavior.interfaces import IInstanceBehaviorAssignableCo
 from imio.smartweb.core.contents import IPages
 from imio.smartweb.core.contents import Pages
 from zope.interface import implementer
+from imio.smartweb.locales import SmartwebMessageFactory as _
+from plone.supermodel import model
+from zope import schema
 
+class IPage(IPages, model.Schema):
+     """Marker interface and Dexterity Python Schema for Page"""
 
-class IPage(IPages):
-    """Marker interface and Dexterity Python Schema for Page"""
+     model.fieldset("layout", label=_("Layout"), fields=["text_align_container"])
+     text_align_container = schema.Bool(
+         title=_("Align title with text"),
+         required=False,
+         default=False,
+     )
 
 
 @implementer(IPage, IInstanceBehaviorAssignableContent)
