493438dc8693f73624e122df9c72ee458e1fb5a2
thomlamb <lambertthomas10@gmail.com>
2026-03-13 14:35:33 +0100

    Fix react add button

--- src/imio/smartweb/core/webcomponents/src/components/News/News.jsx
+++ src/imio/smartweb/core/webcomponents/src/components/News/News.jsx
@@ -154,42 +154,52 @@ const NewsView = (props) => {
                                             activeFilter={filters}
                                             onChange={filtersChange}
                                         />
-                                        {props.proposeUrl && (
-                                            <div className="r-add-news">
-                                                <a target="_blank" href={props.proposeUrl}>
-                                                    <Translate text="Proposer une actualité" />
-                                                    <svg
-                                                        xmlns="http://www.w3.org/2000/svg"
-                                                        width="16"
-                                                        height="16"
-                                                        fill="currentColor"
-                                                        className="bi bi-plus-circle"
-                                                        viewBox="0 0 16 16"
-                                                    >
-                                                        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
-                                                        <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4" />
-                                                    </svg>
-                                                </a>
-                                            </div>
-                                        )}
+                                        <div className="r-result-numbers-and-add-container">
+                                            {itemsNumber > 0 ? (
+                                                <p
+                                                    role="status"
+                                                    aria-live="polite"
+                                                    className="r-results-numbers"
+                                                    aria-label={
+                                                        itemsNumber > 1
+                                                            ? `${itemsNumber} actualités trouvées`
+                                                            : `${itemsNumber} actualité trouvée`
+                                                    }
+                                                >
+                                                    <span aria-hidden="true">{itemsNumber}</span>
+                                                    {itemsNumber > 1
+                                                        ? " actualités trouvées"
+                                                        : " actualité trouvée"}
+                                                </p>
+                                            ) : (
+                                                <p
+                                                    role="status"
+                                                    aria-live="polite"
+                                                    className="r-results-numbers"
+                                                >
+                                                    <Translate text="Aucun résultat" />
+                                                </p>
+                                            )}
+                                            {props.proposeUrl && (
+                                                <div className="r-add-news">
+                                                    <a target="_blank" href={props.proposeUrl}>
+                                                        <Translate text="Proposer une actualité" />
+                                                        <svg
+                                                            xmlns="http://www.w3.org/2000/svg"
+                                                            width="16"
+                                                            height="16"
+                                                            fill="currentColor"
+                                                            className="bi bi-plus-circle"
+                                                            viewBox="0 0 16 16"
+                                                        >
+                                                            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
+                                                            <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4" />
+                                                        </svg>
+                                                    </a>
+                                                </div>
+                                            )}
+                                        </div>
                                     </div>
-                                    {itemsNumber > 0 ? (
-                                        <p
-                                        role="status"
-                                        aria-live="polite"
-                                        className="r-results-numbers"
-                                        aria-label={
-                                            itemsNumber > 1
-                                            ? `${itemsNumber} actualités trouvées`
-                                            : `${itemsNumber} actualité trouvée`
-                                        }
-                                        >
-                                        <span aria-hidden="true">{itemsNumber}</span>
-                                        {itemsNumber > 1 ? " actualités trouvées" : " actualité trouvée"}
-                                        </p>
-                                    ) : (
-                                        ""
-                                    )}
                                     <div>{itemsArray !== null ? listRender : divLoader}</div>
                                     <div className="r-load-more">
                                         {itemsNumber - props.batchSize > batchStart ? (
